25b1f1b567fa025a3d257f2f031deb0f7e5929ec
Jonas <Usuario@CPD02-M08>
2024-08-20 11:57:50 -0300

    Adicionar arquivos de projeto.

---
+++ CombinationBox/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace CombinationBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbTexto = new System.Windows.Forms.TextBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.cbxLista = new System.Windows.Forms.ComboBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.tbIndice = new System.Windows.Forms.TextBox();
+            this.tbTextoCombo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbQuantidade = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbTexto
+            // 
+            this.tbTexto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTexto.Location = new System.Drawing.Point(119, 32);
+            this.tbTexto.Name = "tbTexto";
+            this.tbTexto.Size = new System.Drawing.Size(567, 29);
+            this.tbTexto.TabIndex = 0;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.Location = new System.Drawing.Point(134, 97);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(200, 65);
+            this.btnInsert.TabIndex = 1;
+            this.btnInsert.Text = "Inserir No Combo";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSort.Location = new System.Drawing.Point(475, 97);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(200, 65);
+            this.btnSort.TabIndex = 2;
+            this.btnSort.Text = "Ordenar o Combo";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // cbxLista
+            // 
+            this.cbxLista.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxLista.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxLista.FormattingEnabled = true;
+            this.cbxLista.Location = new System.Drawing.Point(119, 214);
+            this.cbxLista.Name = "cbxLista";
+            this.cbxLista.Size = new System.Drawing.Size(567, 32);
+            this.cbxLista.TabIndex = 3;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemove.Location = new System.Drawing.Point(291, 285);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(223, 65);
+            this.btnRemove.TabIndex = 4;
+            this.btnRemove.Text = "Remover do Combo";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShow.Location = new System.Drawing.Point(291, 409);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(223, 65);
+            this.btnShow.TabIndex = 5;
+            this.btnShow.Text = "Exibir dados do combo";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // tbIndice
+            // 
+            this.tbIndice.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbIndice.Location = new System.Drawing.Point(173, 525);
+            this.tbIndice.Name = "tbIndice";
+            this.tbIndice.Size = new System.Drawing.Size(521, 29);
+            this.tbIndice.TabIndex = 6;
+            // 
+            // tbTextoCombo
+            // 
+            this.tbTextoCombo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTextoCombo.Location = new System.Drawing.Point(173, 591);
+            this.tbTextoCombo.Name = "tbTextoCombo";
+            this.tbTextoCombo.Size = new System.Drawing.Size(521, 29);
+            this.tbTextoCombo.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(53, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 24);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Texto:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(53, 217);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 24);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Lista:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(10, 528);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(157, 24);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Indíce do combo:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 596);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(154, 24);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Texto do combo:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(33, 367);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(193, 24);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Quantidade dos itens:";
+            // 
+            // lbQuantidade
+            // 
+            this.lbQuantidade.AutoSize = true;
+            this.lbQuantidade.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbQuantidade.Location = new System.Drawing.Point(232, 367);
+            this.lbQuantidade.Name = "lbQuantidade";
+            this.lbQuantidade.Size = new System.Drawing.Size(20, 24);
+            this.lbQuantidade.TabIndex = 13;
+            this.lbQuantidade.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 679);
+            this.Controls.Add(this.lbQuantidade);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbTextoCombo);
+            this.Controls.Add(this.tbIndice);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.cbxLista);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.tbTexto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbTexto;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.ComboBox cbxLista;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.TextBox tbIndice;
+        private System.Windows.Forms.TextBox tbTextoCombo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbQuantidade;
+    }
+}
+
---
+++ CombinationBox/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CombinationBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+       
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            cbxLista.Items.Add(tbTexto.Text);
+            tbTexto.Text = "";
+            lbQuantidade.Text = cbxLista.Items.Count.ToString();
+            cbxLista.Select();
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            if (cbxLista.Sorted == false)
+            {
+                cbxLista.Sorted = true;
+            }
+            else
+            {
+                cbxLista.Sorted = false;
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (cbxLista.SelectedIndex != -1)
+            {
+                cbxLista.Items.RemoveAt(int.Parse(tbIndice.Text));
+            }
+            lbQuantidade.Text = cbxLista.Items.Count.ToString();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            if (cbxLista.SelectedIndex != -1)
+            {
+                tbTextoCombo.Text= cbxLista.SelectedItem.ToString();
+                tbIndice.Text= cbxLista.SelectedIndex.ToString();
+            }
+        }
+
+        
+    }
+}
